4639af883a3ab4c90ce952a17939b12cdacbdd0d
TechChats <vigneshmvigi@gmail.com>
2023-03-21 20:19:42 +0530

    Created Login endpoint | Sending auth token back to user when user provides valid email and password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,28 +8,22 @@ const jwt = require('jsonwebtoken');
 //here JWT_SECRET is a secret key used to check weather anyone has tampered the token.
 const JWT_SECRET = 'mySecret';
 
-router.post('/createuser',
+//createUser using POST: "/api/auth/createuser" no login required
+router.post('/createuser', [
 
     //validation
     body('name').isLength({ min: 3 }).withMessage('Name must be at least 3 chars long'),
     body('email').isEmail().withMessage('Email must be a valid email'),
-    body('password').isLength({ min: 5 }).withMessage('Name must be at least 5 chars long').matches(/\d/).withMessage('must contain a number'),
-
-
+    body('password').isLength({ min: 5 }).withMessage('Name must be at least 5 chars long').matches(/\d/).withMessage('must contain a number')],
     async (req, res) => {
 
-        //if error-> return error
+        //if error-> return error and bad request(500)
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        // res.send(req.body)
-        // const user = User(req.body)
-        // user.save()
-
         try {
-
             //check weather user with same email exists if not set a bad request status (400) also error message
             let user = await User.findOne({ email: req.body.email })
             // console.log(user)
@@ -51,7 +45,6 @@ router.post('/createuser',
 
             // console.log(req.body)
 
-
             //here instead of just sending userData as response to user we give token to the user for authentication perpose, once the user has initially authiticated using his email and password in response we give authentication token, so that form next time he can use this token to authenticate
             // res.json(user)
 
@@ -61,7 +54,7 @@ router.post('/createuser',
                 }
             }
             //here JWT_SECRET is a secret key used to check weather anyone has tampered the token.
-            const authToken = jwt.sign(data, JWT_SECRET);
+            const authToken = jwt.sign(data, JWT_SECRET); //sign the token
             console.log(authToken)
             res.json({ authToken })
 
@@ -71,5 +64,56 @@ router.post('/createuser',
             res.status(500).send("error occured")
         }
     })
+
+//Authenticate a user using POST: "/api/auth/login" no login required*
+router.post('/login', [
+    //validation
+    body('email').isEmail().withMessage('Email a valid email'),
+    body('password').exists().withMessage('Password cannot be blank')
+], async (req, res) => {
+    //if error-> return error and bad request(400)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+
+        let user = await User.findOne({ email }) // ie let user = await User.findOne({ email: req.body.email })
+
+        if (!user) {
+            return res.status(400).json({ error: "User with this email is not found! Please Signup" })
+        }
+
+        const comparePassword = await bcrypt.compare(password, user.password)
+
+        if (!comparePassword) {
+            return res.status(400).json({ error: "Password is incorrect" })
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        //here JWT_SECRET is a secret key used to check weather anyone has tampered the token.
+        const authToken = jwt.sign(payload, JWT_SECRET); //sign the token
+        console.log(authToken)
+        res.json({ authToken }) // res.json({ authToken: authToken })
+
+
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("error occured")
+    }
+
+
+}
+
+)
+
+
 
 module.exports = router
