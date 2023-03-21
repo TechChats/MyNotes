2bedbf9e7530bf7792b5ab2bd88bf893713d7cbe
TechChats <vigneshmvigi@gmail.com>
2023-03-21 14:19:45 +0530

    Refactoring and setting thunderclient collections

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose')
 const mongoURI = "mongodb://localhost:27017/my_notes"
 
 const connectToMongo = async ()=> {
-    await mongoose.connect(mongoURI).then((mes)=>{
+    await mongoose.connect(mongoURI)
         //check if db is connected
-        console.log(mes.connection.readyState)
-    })
+        // console.log(mongoose.connection.readyState)
+        if(mongoose.connection.readyState === 1)
+            console.log("Connected to Database")   
 }
 
 module.exports = connectToMongo;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { body, validationResult } = require('express-validator');
 
 
-router.post('/',
+router.post('/createuser',
 
     //validation
     body('name').isLength({ min: 3 }).withMessage('Name must be at least 3 chars long'),
@@ -12,27 +12,41 @@ router.post('/',
     body('password').isLength({ min: 5 }).withMessage('Name must be at least 5 chars long').matches(/\d/).withMessage('must contain a number'),
 
 
-    (req, res) => {
+    async (req, res) => {
 
+        //if error-> return error
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
+            return res.status(400).json({ errors: errors.array() });
         }
 
-        console.log(req.body)
         // res.send(req.body)
         // const user = User(req.body)
         // user.save()
 
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-          }).then(user => res.json(user)).catch(error =>{
-            console.log(error)
-            res.json({ Message: 'Please enter unique email id', Error: error.message})
-          });
+        try {
 
+            //check weather user with same email exists if not set a bad request status (400) also error message
+            let user = await User.findOne({ email: req.body.email })
+            // console.log(user)
+
+            if (user) {
+                return res.status(400).json({ error: "User with the same email exists" })
+            }
+
+            //creating user
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+            })
+
+            console.log(req.body)
+            res.json(user)
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).send("error occured")
+        }
     })
 
 module.exports = router
