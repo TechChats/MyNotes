86641fb4f5b1b02c182fb7832fd8ab4b83171002
TechChats <vigneshmvigi@gmail.com>
2023-03-22 13:33:42 +0530

    Created endpoint to update an existing note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -5,7 +5,7 @@ const fetchuser = require('./../middleware/fetchUser')
 const Note = require('../models/Note')
 
 
-// ROUTE 1: Get All notes of a user using: GET "/api/auth/fetchallnotes" - Login required
+// ROUTE 1: Get All notes of a user using: GET "/api/notes/fetchallnotes" - Login required
 router.get('/fetchallnotes', fetchuser,
     async (req, res) => {
         try {
@@ -20,17 +20,17 @@ router.get('/fetchallnotes', fetchuser,
 
     })
 
-// ROUTE 1: Add notes for a user using: POST "/api/auth/addnote" - Login required
-router.post('/addnote', fetchuser, 
+// ROUTE 2: Add notes for a user using: POST "/api/notes/addnote" - Login required
+router.post('/addnote', fetchuser,
     //validation
     body('title').isLength({ min: 3 }).withMessage('Title must be at least 3 chars long'),
     body('description').isLength({ min: 5 }).withMessage('Description must be at least 5 chars long'),
 
     async (req, res) => {
 
-        const {title, description, tag} = req.body;
+        const { title, description, tag } = req.body;
 
-        
+
         //if error-> return error and bad request(500)
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -52,5 +52,47 @@ router.post('/addnote', fetchuser,
         }
 
     })
+
+// ROUTE 3: Upadate existing notes for a user using: PUT "/api/notes/updatenote" - Login required
+router.put('/updatenote/:id', fetchuser,
+    async (req, res) => {
+
+        const { title, description, tag } = req.body;
+
+        try {
+
+            //create a newNote object and add title, description, tag etc accodingly if these exists
+            //i.e if users just want to update the descrition and tag without changing the title he can do it.
+            const newNote = {}
+
+            if (title) { newNote.title = title } // if title is present in req body i.e if use want to change the title then add it to newNote obj (updated note).
+
+            if (description) { newNote.description = description }
+            if (tag) { newNote.tag = tag }
+
+            //find the note to be updated
+            let note = await Note.findById(req.params.id) // id from -> /updatenote/:id
+
+            //if not present then send page not found request
+            if (!note) {
+                return res.status(404).send("Note note Found")
+            }
+
+            //now once note is present, then check if the user logged in (whose is trying to update the note) is the original user who had created that note.
+            if(note.user.toString() !== req.user.id){
+                return res.status(401).send("Not Allowed to update")
+            }
+
+            note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true} ) // new:true ??
+            res.json(note)
+
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).send("error occured")
+        }
+
+    })
+
+
 
 module.exports = router
